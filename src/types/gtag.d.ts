710600d73c0469975421953e17c86e42e23b32c4
coring2yo <prompot7@gmail.com>
2025-09-04 01:37:21 +0900

    애널리틱스 코드 추가

---
+++ src/types/gtag.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  interface Window {
+    gtag: (
+      command: 'config' | 'event' | 'js',
+      targetId: string,
+      config?: {
+        page_path?: string;
+        [key: string]: any;
+      }
+    ) => void;
+    dataLayer: any[];
+  }
+}
+
+export {};
