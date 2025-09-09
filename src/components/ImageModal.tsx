04136597e4ebec4c14cd507e8dc501fd8559e224
coring2yo <prompot7@gmail.com>
2025-09-09 16:35:18 +0900

    feat: 이미지 모달 UI 간소화
    - 좌상단 확대 아이콘 제거
    - 하단 안내 문구 제거
    - 깔끔한 이미지 확대 모달로 개선
    - 닫기 버튼만 유지하여 UI 정리

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -52,13 +52,6 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageUrl, alt 
           </svg>
         </button>
 
-        {/* 확대 아이콘 */}
-        <div className="absolute top-4 left-4 z-10 p-2 bg-black/50 text-white rounded-full">
-          <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
-          </svg>
-        </div>
-
         {/* 이미지 */}
         <div className="relative w-full h-full flex items-center justify-center">
           <Image
@@ -70,11 +63,6 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageUrl, alt 
             unoptimized={true}
             priority
           />
-        </div>
-
-        {/* 하단 안내 텍스트 */}
-        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10 px-4 py-2 bg-black/50 text-white text-sm rounded-full">
-          ESC 키 또는 배경 클릭으로 닫기
         </div>
       </div>
     </div>
